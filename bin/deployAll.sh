cfd325a1c351bd376f8e32e215957a5af9fdb7b8
Jon Green <jgreen@izimobile.com>
2016-08-22 14:31:36 -0400

    Switching back to webpack. - Updating lambda template for the API changes made in node4.3 update. - Updating build scripts to allow multiple lambdas to be processed at once.

--- bin/deployAll.sh
+++ bin/deployAll.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 npm run clean
-
 for file in $(ls ./src/lambdas); do
   export APP=$(echo "$file" | sed 's/.js$//g')
-  npm run rollup && \
-  npm run gulp && \
-  rm -rf tmp
+  npm run webpack && \
+  npm run gulp
 done
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,41 @@
 var gulp = require('gulp'),
-    zip = require('gulp-zip'),
-    del = require('del'),
-    runSequence = require('run-sequence'),
-    lambda = require('node-aws-lambda'),
-    rimraf = require('rimraf');
+  zip = require('gulp-zip'),
+  del = require('del'),
+  install = require('gulp-install'),
+  runSequence = require('run-sequence'),
+  lambda = require('node-aws-lambda'),
+  rimraf = require('rimraf');
 
 const zipped = `./tmp/${process.env.APP}.zip`;
 
 //load release-it tasks
 require('gulp-release-it')(gulp);
 
-gulp.task('zip', () => {
-    return gulp.src(`./dest/lambdas/${process.env.APP}/**/*`)
-        .pipe(zip(`${process.env.APP}.zip`))
-        .pipe(gulp.dest('./tmp'));
+gulp.task('modules', _ => {
+  return gulp.src('./package.json')
+    .pipe(gulp.dest(`dist/lambdas/${process.env.APP}`))
+    .pipe(install({ production: true }));
+});
+
+gulp.task('zip', _ => {
+  return gulp.src(`./dist/lambdas/${process.env.APP}/**/*`)
+    .pipe(zip(`${process.env.APP}.zip`))
+    .pipe(gulp.dest('./tmp'));
 });
 
 gulp.task('upload', (callback) => {
-    lambda.deploy(
-      zipped,
-      require(`./config/${process.env.APP}.config.js`),
-      callback
-    );
+  lambda.deploy(
+    zipped,
+    require(`./config/${process.env.APP}.config.js`),
+    callback
+  );
 });
 
 gulp.task('deploy', (callback) => {
-    return runSequence(
-        ['zip'],
-        ['upload'],
-        callback
-    );
+  return runSequence(
+    ['modules'],
+    ['zip'],
+    ['upload'],
+    callback
+  );
 });
